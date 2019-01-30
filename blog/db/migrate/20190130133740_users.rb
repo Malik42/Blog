4e68ad29ffea67059de947857cba5725e0d00d4e
Malik42 <malik.ahres@hotmail.fr>
2019-01-30 15:07:18 +0100

    testing bdd

--- blog/db/migrate/20190130133740_users.rb
+++ blog/db/migrate/20190130133740_users.rb
@@ -1,4 +1,10 @@
 class Users < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.integer :id
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.timestamps
   end
 end
