20990e40909586debc9c423ec25d7d2c0d054184
Lorenzo <35255798+modeuil@users.noreply.github.com>
2019-01-31 04:40:44 +0100

    Update 20190130230842_add_index_to_articles.rb

--- db/migrate/20190130230842_add_index_to_articles.rb
+++ db/migrate/20190130230842_add_index_to_articles.rb
@@ -1,5 +1,6 @@
 class AddIndexToArticles < ActiveRecord::Migration[5.2]
   def change
+    # add colum with id (foreign key)
     add_column :articles, :user_id, :integer
     add_index :articles, :user_id
   end
