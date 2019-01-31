6de32d56c7583513ef78f1e4824e7d410d98f32d
Lorenzo <35255798+modeuil@users.noreply.github.com>
2019-01-31 04:41:40 +0100

    Update 20190130162408_create_users.rb

--- db/migrate/20190130162408_create_users.rb
+++ db/migrate/20190130162408_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      #Init les catégories des tables
       t.string :first_name
       t.string :last_name
       t.string :email
