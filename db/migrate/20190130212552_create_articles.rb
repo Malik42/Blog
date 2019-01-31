5dc231f928ed17964016a37ddf9cdd2c673dc249
Modeuil <votre_email@votre_email.com>
2019-01-31 02:04:40 +0100

    all modification

--- db/migrate/20190130212552_create_articles.rb
+++ db/migrate/20190130212552_create_articles.rb
@@ -1,8 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
+      t.string :title
+      t.text :content
       t.timestamps
+      add_reference :articles, :user, foreign_key: true
     end
   end
+
+
 end
