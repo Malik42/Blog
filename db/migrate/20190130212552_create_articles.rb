7b35711391eacee83ff910e84cfb3a52900d2031
Lorenzo <35255798+modeuil@users.noreply.github.com>
2019-01-31 04:42:34 +0100

    Update 20190130212552_create_articles.rb

--- db/migrate/20190130212552_create_articles.rb
+++ db/migrate/20190130212552_create_articles.rb
@@ -1,9 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      #initialise les catégories des tables
       t.string :title
       t.text :content
       t.timestamps
+      # permet de lier 2 tables déjà existante
       add_reference :articles, :user, foreign_key: true
     end
   end
